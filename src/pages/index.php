fca9c31e05c75238ff9c810d27b77f8bcf92f73f
Nico Watine <nico@studioncreations.com>
2019-04-01 08:09:15 -0500

    Tweak some margins

--- src/pages/index.php
+++ src/pages/index.php
@@ -167,9 +167,9 @@ body-class:
 			<section class="tanspire-level-club-section">
 				<div class="grid-x grid-padding-x">
 					<div class="cell intro-prose py-40">
-						<h1 class="title text-center">Tanspire Level&nbsp;Club</h1>
+						<h1 class="title text-center mb-16">Tanspire Level&nbsp;Club</h1>
 						<div class="prose-wrap text-left">
-							<p>The lowest-priced way to tan is always our <strong>TS&nbsp;Level&nbsp;Club&nbsp;membership</strong>. <br>Enjoy monthly unlimited tanning with beautiful&nbsp;benefits. <br><strong>Limited time only!</strong></p>
+							<p class="m-0">The lowest-priced way to tan is always our <strong>TS&nbsp;Level&nbsp;Club&nbsp;membership</strong>. <br>Enjoy monthly unlimited tanning with beautiful&nbsp;benefits. <br><strong>Limited time only!</strong></p>
 						</div>
 					</div>
 				</div>
