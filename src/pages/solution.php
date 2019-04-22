5b1bb2eae510e093e79bd44b6def5ff9da4b3ac7
Nico Watine <nico@studioncreations.com>
2019-04-22 17:14:26 -0500

    Progress merging sections

--- src/pages/solution.php
+++ src/pages/solution.php
@@ -14,13 +14,13 @@ body-class:
 			<!-- Tanspire Glow Solution -->
 			<section class="tanspire-glow-solution-section py-40 px-8">
 				<div class="grid-x grid-padding-x align-middle">
-					<div class="cell large-6 large-order-2">
+					<div class="cell text-block large-6 large-order-2">
 						<h3 class="h3 mb-16 title">The TanSpire Glow&nbsp;Solution</h3>
 						<div class="bounding-box">
 							<p class="lead"><strong>All Signature Tans are applied with our custom tanning&nbsp;solution.</strong></p>
 							<p><strong>We apply our own unique tanning solution, developed in-house and available exclusively for TanSpire clients.</strong> The solution is infused with naturally-rich antioxidants, nourishing your skin while providing a healthy tan that is slow-fading. This is skin treatment in a personalized airbrush&nbsp;tan.</p>
 						</div>
-						<div class="callout custom-callout p-16 box-shadow-1 text-center">
+						<div class="show-for-large callout custom-callout p-16 box-shadow-1 text-center">
 							<div class="content text-left display-inline-block">
 								<p class="overflow-hidden callout-heading strong mb-8"><span>Beneficial Attributes</span></p>
 								<ul class="mb-0">
@@ -35,8 +35,24 @@ body-class:
 						</div>
 					</div>
 					<div class="cell large-6 large-order-1 align-self-top">
-						<div class="img-block text-center">
-							<img class="overflow-hidden" style="border-radius: 8px;" src="/img/index/tanspire-glow-solution/tanspire-glow-solution-feature-model-large.jpg" alt="">
+						<div class="img-block text-center mb-32">
+							<img class="show-for-large" style="border-radius: 8px;" src="/img/index/tanspire-glow-solution/tanspire-glow-solution-feature-model-large.jpg" alt="">
+							<img class="hide-for-large" style="border-radius: 8px;" src="/img/index/tanspire-solution/tanspire-solution-feature-image.jpg" alt="">
+						</div>
+					</div>
+					<div class="cell hide-for-large">
+						<div class="callout custom-callout p-16 box-shadow-1 text-center">
+							<div class="content text-left display-inline-block">
+								<p class="overflow-hidden callout-heading strong mb-8"><span>Beneficial Attributes</span></p>
+								<ul class="mb-0">
+									<li>pH Balanced for optimal color development</li>
+									<li>Provides your skin with a Vitamin E boost</li>
+									<li>Completely odorless and non-sticky</li>
+									<li>No Alcohol, No Parabens and No Formaldehyde odors</li>
+									<li>Vegan and Cruelty Free</li>
+								</ul>
+							</div>
+							<div class="cf"></div>
 						</div>
 					</div>
 				</div>
