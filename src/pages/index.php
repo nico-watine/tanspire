974efc98df117b5cffb73ea97bf3d6f345092421
Nico Watine <nico@studioncreations.com>
2019-04-22 17:25:41 -0500

    Rearrange ownership of mb

--- src/pages/index.php
+++ src/pages/index.php
@@ -170,29 +170,49 @@ body-class:
 					<hr>
 				</div>
 			</div>
-			<!-- Tanspire Solution -->
-			<section class="tanspire-solution-section py-40 px-8">
-				<h3 class="h3 mb-32 title text-center">The TanSpire Solution</h3>
-				<div class="bounding-box" style="max-width: 440px; margin: 0 auto;">
-					<p class="lead mb-16"><strong>All Signature Tans are applied with our custom tanning solution.</strong></p>
-					<p class="mb-32"><strong>We apply our own unique tanning solution, developed in-house and available exclusively for TanSpire clients.</strong> The solution is infused with naturally-rich antioxidants, nourishing your skin while providing a healthy tan that is slow-fading. This is skin treatment in a personalized airbrush tan.</p>
-				</div>
-				<div class="img-block text-center">
-					<img class="overflow-hidden mb-32" style="border-radius: 8px;" src="/img/index/tanspire-solution/tanspire-solution-feature-image.jpg" alt="">
-				</div>
-					<div class="callout custom-callout p-16 box-shadow-1 text-center">
-						<div class="content text-left display-inline-block">
-							<p class="overflow-hidden callout-heading strong mb-8"><span>Beneficial Attributes</span></p>
-							<ul class="mb-0">
-								<li>pH Balanced for optimal color development</li>
-								<li>Provides your skin with a Vitamin E boost</li>
-								<li>Completely odorless and non-sticky</li>
-								<li>No Alcohol, No Parabens and No Formaldehyde odors</li>
-								<li>Vegan and Cruelty Free</li>
-							</ul>
+			<!-- Tanspire Glow Solution -->
+			<section class="tanspire-glow-solution-section py-40 px-8">
+				<div class="grid-x grid-padding-x align-middle">
+					<div class="cell text-block large-6 large-order-2">
+						<h3 class="h3 title text-center large-text-left">The TanSpire Glow&nbsp;Solution</h3>
+						<div class="bounding-box">
+							<p class="lead"><strong>All Signature Tans are applied with our custom tanning solution.</strong></p>
+							<p><strong>We apply our own unique tanning solution, developed in-house and available exclusively for TanSpire clients.</strong> The solution is infused with naturally-rich antioxidants, nourishing your skin while providing a healthy tan that is slow-fading. This is skin treatment in a personalized airbrush&nbsp;tan.</p>
 						</div>
-						<div class="cf"></div>
+						<div class="show-for-large callout custom-callout p-16 box-shadow-1 text-center">
+							<div class="content text-left display-inline-block">
+								<p class="overflow-hidden callout-heading strong mb-8"><span>Beneficial Attributes</span></p>
+								<ul class="mb-0">
+									<li>pH Balanced for optimal color development</li>
+									<li>Provides your skin with a Vitamin E boost</li>
+									<li>Completely odorless and non-sticky</li>
+									<li>No Alcohol, No Parabens and No Formaldehyde odors</li>
+									<li>Vegan and Cruelty Free</li>
+								</ul>
+							</div>
+							<div class="cf"></div>
+						</div>
 					</div>
+					<div class="cell img-block large-6 large-order-1 align-self-top text-center">
+						<img class="show-for-large" src="/img/index/tanspire-glow-solution/tanspire-glow-solution-feature-model-large.jpg" alt="">
+						<img class="hide-for-large mb-32" src="/img/index/tanspire-solution/tanspire-solution-feature-image.jpg" alt="">
+					</div>
+					<div class="cell hide-for-large">
+						<div class="callout custom-callout p-16 box-shadow-1 text-center">
+							<div class="content text-left display-inline-block">
+								<p class="overflow-hidden callout-heading strong mb-8"><span>Beneficial Attributes</span></p>
+								<ul class="mb-0">
+									<li>pH Balanced for optimal color development</li>
+									<li>Provides your skin with a Vitamin E boost</li>
+									<li>Completely odorless and non-sticky</li>
+									<li>No Alcohol, No Parabens and No Formaldehyde odors</li>
+									<li>Vegan and Cruelty Free</li>
+								</ul>
+							</div>
+							<div class="cf"></div>
+						</div>
+					</div>
+				</div>
 			</section>
 		</div>
 	</div>
